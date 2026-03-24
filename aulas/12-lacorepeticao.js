3ceec0ca2ebf2c4c9fd3be6646f16131404950c1
zen918 <davifernandesdasilva88@gmail.com>
2026-03-24 11:10:07 -0300

    aprendendo sobre lacorepeticao em java script

--- aulas/12-lacorepeticao.js
+++ aulas/12-lacorepeticao.js
@@ -2,6 +2,40 @@
 // o professor te colocou de castigo e pediu para voce escrever mil vezes a frase
 // "eu vou prestar atenção as aulas e anotar tudo"
 
-for (let i = 0; i < 1000; i++) { // i++ == 1 = i+1
-    
+// let i = 0 => primeiro utilizo uma variavel de controle
+// i < 1000 => condição de repetição
+// i++ => aumenta a variavel de controle para não travar e somar
+for (let i = 0; i < 1000; i++) {
+  console.log("eu vou prestar atenção as aulas e anotar tudo!");
 }
+console.log("----------------------------------------------------------");
+
+// escreva todos os numeros de 1 a 20
+for (let i = 1; i <= 20; i++) {
+  console.log(i);
+}
+
+// escreva todos os numeros pares entre 1 a 20
+for (let i = 0; i <= 20; i += 2) {
+  console.log(i);
+}
+// outra solução de numeros pares
+for (let i = 0; i <= 20; i++) {
+  if (i % 2 == 0) console.log(i);
+  // % => resto da divisão
+  // i % 2 == 0 => se o resto da divisão de 1 por 2 for 0, ele mostra i
+}
+
+
+console.log("----------------------------------------------------------");
+// dado um vetor, calcule e exiba o somatorio de seus elementos
+var numeros = [5, 6, 8, 14, 0, 6, 9, 7, 2];
+let soma = 0;
+
+console.log(numeros.length);
+//numeros,length => mostra a quantidade de itens do array
+
+for (let i = 0; i < numeros.length; i++) {
+  soma += numeros[i];
+}
+console.log(soma);
