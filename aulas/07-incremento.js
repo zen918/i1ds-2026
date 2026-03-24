17a6c6cb5c2fe307d35e312bdac407b0cf05896f
zen918 <davifernandesdasilva88@gmail.com>
2026-03-24 10:08:49 -0300

    aprendendo sobre incrementos e começo de laco de repeticao

---
+++ aulas/07-incremento.js
@@ -0,0 +1,30 @@
+// operadores de incremento e decremento | pre e pos incrementos
+// incremento => somar algo de alguma variavel
+// decremento => subtrair algo de alguma variavel
+
+let idade = 16;
+console.log(idade);
+
+//idade = idade + 1; // somei 1 na idade
+idade++;
+console.log(idade);
+idade++;
+console.log(idade);
+console.log("--------------------------------------------------------");
+
+//idade = idade - 1; // subtrai 1 na idade
+idade--;
+console.log(idade);
+idade--;
+console.log(idade);
+console.log("--------------------------------------------------------");
+
+let novaidade = idade + 1; // idade++ soma na mesma variavel
+console.log("idade:", idade,"| nova idade:", novaidade);
+
+novaidade = idade++;
+console.log("idade:", idade,"| nova idade:", novaidade);
+
+
+
+
---
+++ aulas/12-lacorepeticao.js
@@ -0,0 +1,7 @@
+//       laço de repetição -  FOR (PARA)
+// o professor te colocou de castigo e pediu para voce escrever mil vezes a frase
+// "eu vou prestar atenção as aulas e anotar tudo"
+
+for (let i = 0; i < 1000; i++) { // i++ == 1 = i+1
+    
+}
