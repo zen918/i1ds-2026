cbb4e274901c3326469ee9802e80f858a55ca677
zen918 <davifernandesdasilva88@gmail.com>
2026-03-24 16:56:10 -0300

    atividade sobre matriz

---
+++ atividades/08-atividade.js
@@ -0,0 +1,46 @@
+/*****************************************************
+Lista de Exercícios
+*****************************************************/
+/*
+Dada a matriz abaixo, faça um programa em JavaScript
+que imprima os elementos da sua diagonal principal.
+| 3 5 8 |
+| 1 9 2 |
+| 7 1 4 |
+********************************************************
+Dada a matriz abaixo, faça um programa em JavaScript
+que multiplique seus elementos por um valor x e imprima
+a matriz antes e após a multiplicação.
+| 8 1 3 |
+| 2 9 1 |
+| 4 6 2 |
+*/
+
+var matriz = [
+  [3, 5, 8],
+  [1, 9, 2],
+  [7, 1, 4],
+];
+console.table(matriz);
+
+console.log(matriz[0][0]);
+console.log(matriz[1][1]);
+console.log(matriz[2][2]);
+console.log(matriz[0][2]);
+console.log(matriz[1][1]);
+console.log(matriz[2][0]);
+
+console.log("-------------------------------------------------------------")
+
+var matriz1 = [
+  [8, 1, 3],
+  [2, 9, 1],
+  [4, 6, 2],
+];
+console.table(matriz1);
+
+for (let i = 0; i < matriz1; i++) {
+  matriz1[i] *= 4; 
+}
+console.table(matriz1);
+
