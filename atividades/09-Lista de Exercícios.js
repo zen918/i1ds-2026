c1f968efe34c76ec3dd0210d8ae0547abe9b0977
zen918 <davifernandesdasilva88@gmail.com>
2026-03-25 11:07:59 -0300

    lista de exercicios

---
+++ atividades/09-Lista de Exercícios.js
@@ -0,0 +1,51 @@
+console.log("/--------------parte 01-----------------------/")
+var nome = "robson";
+var idade = 36;
+var cidade = "jau"
+
+//Demonstrando no console várias informações e pulo de linha com \n
+console.log("Meu nome é", nome, "e eu tenho", idade, "anos\n", "e moro em", cidade);
+console.log("-------------------------------------------------------------------------")
+
+let num1 = 45;
+let num2 = 5;
+
+let soma = num1 + num2;
+let subtracao = num1 - num2;
+let multiplicacao = num1 * num2;
+let divisao = num1 / num2;
+let restoDivisao = num1 % num2;
+
+console.log("Somando", num1, " + ", num2, " = ", soma);
+console.log("subtraindo", num1, " - ", num2, " = ", subtracao);
+console.log("multiplicando", num1, " * ", num2, " = ", multiplicacao);
+console.log("dididindo", num1, " / ", num2, " = ", divisao);
+console.log("O Resto da Divisão do", num1, " % ", num2, " = ", restoDivisao);
+console.log("-------------------------------------------------------------------------")
+
+let comprimento = 10
+let altura = 3
+let area = comprimento *altura
+
+console.log("se a altura do retangulo é", altura, "e o comprimento é de", comprimento, "a area é =", area )
+console.log("---------------------------------------------------------------------------------------------")
+
+let C = 10
+let L = 5
+let H = 3
+let volume = C * L * H
+console.log("se o comprimento do paralelepípedo é", C,)
+console.log("e a largura é", L,)
+console.log("e a altura é", H)
+console.log("o volume é =",volume )
+console.log("-------------------------------------------------------------------------")
+
+let valor = 100
+let desconto = 10/100
+
+
+
+
+
+
+
