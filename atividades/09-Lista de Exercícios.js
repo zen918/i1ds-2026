b9b4ad229215c1b17d07926142d170f1928f48b0
zen918 <davifernandesdasilva88@gmail.com>
2026-03-25 16:33:52 -0300

    lista de exercicios

--- atividades/09-Lista de Exercícios.js
+++ atividades/09-Lista de Exercícios.js
@@ -11,10 +11,10 @@ let num1 = 45;
 let num2 = 5;
 
 let soma = num1 + num2;
-let subtracao = num1 - num2;
-let multiplicacao = num1 * num2;
-let divisao = num1 / num2;
-let restoDivisao = num1 % num2;
+var subtracao = num1 - num2;
+var multiplicacao = num1 * num2;
+var divisao = num1 / num2;
+var restoDivisao = num1 % num2;
 
 console.log("Somando", num1, " + ", num2, " = ", soma);
 console.log("subtraindo", num1, " - ", num2, " = ", subtracao);
@@ -23,36 +23,66 @@ console.log("dididindo", num1, " / ", num2, " = ", divisao);
 console.log("O Resto da Divisão do", num1, " % ", num2, " = ", restoDivisao);
 console.log("-------------------------------------------------------------------------")
 
-let comprimento = 10
-let altura = 3
-let area = comprimento *altura
+var comprimento = 10
+var altura = 3
+var area = comprimento *altura
 
 console.log("se a altura do retangulo é", altura, "e o comprimento é de", comprimento, "a area é =", area )
 console.log("---------------------------------------------------------------------------------------------")
 
-let C = 10
-let L = 5
-let H = 3
-let volume = C * L * H
+var C = 10
+var L = 5
+var H = 3
+var volume = C * L * H
 console.log("se o comprimento do paralelepípedo é", C,)
 console.log("e a largura é", L,)
 console.log("e a altura é", H)
 console.log("o volume é =",volume )
 console.log("-------------------------------------------------------------------------")
 
-let valor = 100
-let desconto = 10/100
-let valorfinal = valor -(valor*desconto)
+var valor = 100
+var desconto = 10/100
+var valorfinal = valor -(valor*desconto)
  console.log ("valor do produto é de", valor)
  console.log ("alicando desconto de", desconto,"%")
  console.log ("total da compra é de", valorfinal)
 console.log("-------------------------------------------------------------------------")
 
-let celcius = 20
-let fahrenheit = celcius * 1.8 + 32
+var celcius = 20
+var fahrenheit = celcius * 1.8 + 32
 
 console.log (celcius,"graus celcius é igual a", fahrenheit,"graus fahrenheit")
 console.log("-------------------------------------------------------------------------")
+
+var altura = 1.75
+var peso = 65
+var imc = peso / (altura * altura)
+ console.log("uma pessoa que mede", altura, "e pesa", peso,"kg tem o imc =", imc)
+console.log("-------------------------------------------------------------------------")
+
+var nota = 7
+var nota2 = 7
+var nota3 = 7
+var media = (nota + nota2 + nota3 )/ 3
+console.log ("a media final do aluno que tirou as notas", nota, nota2, nota3, "é de =", media)
+console.log("---------------------------------------------------------------------------------")
+
+console.log("-------------------------------------------------------------------------")
+
+var hora = 5
+var minutos = hora * 60
+var segundos = hora * 3600
+
+console.log (hora, "horas é igua á", minutos,"min")
+console.log (hora, "horas é igua á", segundos,"seg")
+
+
+
+console.log("/--------------parte 02-----------------------/")
+
+
+
+
 
 
 
