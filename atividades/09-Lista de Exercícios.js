19c2456425f484451a0def6ce4de6ffdf931bc23
zen918 <davifernandesdasilva88@gmail.com>
2026-03-25 16:53:30 -0300

    lista de exercicios

--- atividades/09-Lista de Exercícios.js
+++ atividades/09-Lista de Exercícios.js
@@ -1,11 +1,22 @@
-console.log("/--------------parte 01-----------------------/")
+console.log("/---------------------------------parte 01---------------------------------/");
+console.log("//");
 var nome = "robson";
 var idade = 36;
-var cidade = "jau"
+var cidade = "jau";
 
 //Demonstrando no console várias informações e pulo de linha com \n
-console.log("Meu nome é", nome, "e eu tenho", idade, "anos\n", "e moro em", cidade);
-console.log("-------------------------------------------------------------------------")
+console.log(
+  "Meu nome é",
+  nome,
+  "e eu tenho",
+  idade,
+  "anos\n",
+  "e moro em",
+  cidade,
+);
+console.log(
+  "-------------------------------------------------------------------------",
+);
 
 let num1 = 45;
 let num2 = 5;
@@ -21,72 +32,97 @@ console.log("subtraindo", num1, " - ", num2, " = ", subtracao);
 console.log("multiplicando", num1, " * ", num2, " = ", multiplicacao);
 console.log("dididindo", num1, " / ", num2, " = ", divisao);
 console.log("O Resto da Divisão do", num1, " % ", num2, " = ", restoDivisao);
-console.log("-------------------------------------------------------------------------")
+console.log(
+  "-------------------------------------------------------------------------",
+);
 
-var comprimento = 10
-var altura = 3
-var area = comprimento *altura
+var comprimento = 10;
+var altura = 3;
+var area = comprimento * altura;
 
-console.log("se a altura do retangulo é", altura, "e o comprimento é de", comprimento, "a area é =", area )
-console.log("---------------------------------------------------------------------------------------------")
+console.log(
+  "se a altura do retangulo é",
+  altura,
+  "e o comprimento é de",
+  comprimento,
+  "a area é =",
+  area,
+);
+console.log(
+  "---------------------------------------------------------------------------------------------",
+);
 
-var C = 10
-var L = 5
-var H = 3
-var volume = C * L * H
-console.log("se o comprimento do paralelepípedo é", C,)
-console.log("e a largura é", L,)
-console.log("e a altura é", H)
-console.log("o volume é =",volume )
-console.log("-------------------------------------------------------------------------")
+var C = 10;
+var L = 5;
+var H = 3;
+var volume = C * L * H;
+console.log("se o comprimento do paralelepípedo é", C);
+console.log("e a largura é", L);
+console.log("e a altura é", H);
+console.log("o volume é =", volume);
+console.log(
+  "-------------------------------------------------------------------------",
+);
 
-var valor = 100
-var desconto = 10/100
-var valorfinal = valor -(valor*desconto)
- console.log ("valor do produto é de", valor)
- console.log ("alicando desconto de", desconto,"%")
- console.log ("total da compra é de", valorfinal)
-console.log("-------------------------------------------------------------------------")
+var valor = 100;
+var desconto = 10 / 100;
+var valorfinal = valor - valor * desconto;
+console.log("valor do produto é de", valor);
+console.log("alicando desconto de", desconto, "%");
+console.log("total da compra é de", valorfinal);
+console.log(
+  "-------------------------------------------------------------------------",
+);
 
-var celcius = 20
-var fahrenheit = celcius * 1.8 + 32
+var celcius = 20;
+var fahrenheit = celcius * 1.8 + 32;
 
-console.log (celcius,"graus celcius é igual a", fahrenheit,"graus fahrenheit")
-console.log("-------------------------------------------------------------------------")
+console.log(celcius, "graus celcius é igual a", fahrenheit, "graus fahrenheit");
+console.log(
+  "-------------------------------------------------------------------------",
+);
 
-var altura = 1.75
-var peso = 65
-var imc = peso / (altura * altura)
- console.log("uma pessoa que mede", altura, "e pesa", peso,"kg tem o imc =", imc)
-console.log("-------------------------------------------------------------------------")
+var altura = 1.75;
+var peso = 65;
+var imc = peso / (altura * altura);
+console.log(
+  "uma pessoa que mede",
+  altura,
+  "e pesa",
+  peso,
+  "kg tem o imc =",
+  imc,
+);
+console.log(
+  "-------------------------------------------------------------------------",
+);
 
-var nota = 7
-var nota2 = 7
-var nota3 = 7
-var media = (nota + nota2 + nota3 )/ 3
-console.log ("a media final do aluno que tirou as notas", nota, nota2, nota3, "é de =", media)
-console.log("---------------------------------------------------------------------------------")
+var nota = 7;
+var nota2 = 7;
+var nota3 = 7;
+var media = (nota + nota2 + nota3) / 3;
+console.log(
+  "a media final do aluno que tirou as notas",
+  nota,
+  nota2,
+  nota3,
+  "é de =",
+  media,
+);
+console.log(
+  "---------------------------------------------------------------------------------",
+);
 
-console.log("-------------------------------------------------------------------------")
+console.log(
+  "-------------------------------------------------------------------------",
+);
 
-var hora = 5
-var minutos = hora * 60
-var segundos = hora * 3600
+var hora = 5;
+var minutos = hora * 60;
+var segundos = hora * 3600;
 
-console.log (hora, "horas é igua á", minutos,"min")
-console.log (hora, "horas é igua á", segundos,"seg")
+console.log(hora, "horas é igua á", minutos, "min");
+console.log(hora, "horas é igua á", segundos, "seg");
 
-
-
-console.log("/--------------parte 02-----------------------/")
-
-
-
-
-
-
-
-
-
-
+console.log("/---------------------------------parte 02---------------------------------/");
 
