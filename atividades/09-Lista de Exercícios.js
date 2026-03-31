f5026f223b5074434a4af80697abda2290b7f1cc
zen918 <davifernandesdasilva88@gmail.com>
2026-03-31 15:36:06 -0300

    lista de exercicios

--- atividades/09-Lista de Exercícios.js
+++ atividades/09-Lista de Exercícios.js
@@ -196,8 +196,7 @@ if (num1 % 3 === 0) {
 }
 console.log("----------------------------------------------------------------------")
 
-function recuperação(nota) {
-  let frase;
+function recuperação(nota) {var frase;
   if (nota < 5) {
     frase = "reprovado";
   } else if (nota >= 5 && nota <= 6) {
@@ -207,18 +206,235 @@ function recuperação(nota) {
   }
   return frase;
 }
-var idade = 4;
-console.log("Idade:", idade, "-", recuperação(idade));
-idade = 6;
-console.log("Idade:", idade, "-", recuperação(idade));
-idade = 7;
-console.log("Idade:", idade, "-", recuperação(idade));
-idade = 10;
-console.log("Idade:", idade, "-", recuperação(idade));
+var nota = 4;
+console.log("nota:", nota, "-", recuperação(nota));
+nota = 6;
+console.log("nota:", nota, "-", recuperação(nota));
+nota = 7;
+console.log("nota:", nota, "-", recuperação(nota));
+nota = 10;
+console.log("nota:", nota, "-", recuperação(nota));
 console.log("----------------------------------------------------------------------")
 
+function podeVotar(idade) {
+  var frase;
+  if (idade < 16) {
+    frase = "Não pode Votar";
+  } else if (idade >= 18 && idade <= 69) {
+    frase = "Pode votar, e é obrigatório!";
+  } else {
+    frase = "Pode votar, mas é opcional!";
+  }
+  return frase;
+}
+var idade = 22;
+console.log("Idade:", idade, "-", podeVotar(idade));
+idade = 26;
+console.log("Idade:", idade, "-", podeVotar(idade));
+idade = 13;
+console.log("Idade:", idade, "-", podeVotar(idade));
+idade = 70;
+console.log("Idade:", idade, "-", podeVotar(idade));
+idade = 17;
+console.log("Idade:", idade, "-", podeVotar(idade));
+console.log("----------------------------------------------------------------------")
+
+var num = 25; 
+var num1 = 79; 
+
+if (num >= 10 && num <= 50) {
+    console.log("O número está entre 10 e 50.");
+} else {
+    console.log("O número está fora do intervalo.");
+}
+
+if (num1 >= 10 && num1 <= 50) {
+    console.log("O número está entre 10 e 50.");
+} else {
+    console.log("O número está fora do intervalo.");
+
+}
+console.log("----------------------------------------------------------------------")
+
+var login = "robson";
+var senha = "819";
+
+var loginUser = "adm";
+var senhaUser = "1234";
+
+if (loginUser == login && senhaUser == senha) {
+  console.log("Login Realizado com Sucesso!");
+}
+
+if (loginUser == login && senhaUser == senha) {
+  console.log("Acesso Permitido!");
+} else {
+  console.log("Acesso Negado!");
+}
+
+var loginUser = "robson";
+var senhaUser = "819";
+
+if (loginUser == login && senhaUser == senha) {
+  console.log("Login Realizado com Sucesso!");
+}
+
+if (loginUser == login && senhaUser == senha) {
+  console.log("Acesso Permitido!");
+} else {
+  console.log("Acesso Negado!");
+
+}
+console.log("----------------------------------------------------------------------")
+
+var x = 25;
+var y = 15;
+var maior;
+
+if (x > y) {
+    maior = x;
+} else if (y > x) {
+    maior = y;
+} else {
+    maior = "esse é o maior numero";
+}
+
+console.log("o maior numero é", maior); 
+console.log("----------------------------------------------------------------------")
+
+var a = 10, b = 35, c = 15;
+var maior;
+
+if (a >= b && a >= c) {
+    maior = a;
+} else if (b >= a && b >= c) {
+    maior = b;
+} else {
+    maior = c;
+}
+console.log("O maior é: " + maior);
+console.log("----------------------------------------------------------------------")
+
+function classificarIdade(idade) {
+  if (idade < 12) {
+    return "Criança";
+  } else if (idade < 18) {
+    return "Adolescente";
+  } else if (idade < 60) {
+    return "Adulto";
+  } else {
+    return "Idoso";
+  }
+}
+
+
+console.log(classificarIdade(10));
+console.log(classificarIdade(15)); 
+console.log(classificarIdade(30)); 
+console.log(classificarIdade(65)); 
+console.log("----------------------------------------------------------------------")
+
+console.log("/---------------------------------parte 03---------------------------------/");
+
+function obterDiaSemanaSwitch(numero) {
+    switch (Number(numero)) {
+        case 1: return "Domingo";
+        case 2: return "Segunda-feira";
+        case 3: return "Terça-feira";
+        case 4: return "Quarta-feira";
+        case 5: return "Quinta-feira";
+        case 6: return "Sexta-feira";
+        case 7: return "Sábado";
+        default: return "Valor inválido";
+    }
+}
+
+console.log(obterDiaSemanaSwitch(5)); 
+console.log(obterDiaSemanaSwitch(1)); 
+console.log(obterDiaSemanaSwitch(3)); 
+console.log(obterDiaSemanaSwitch(9)); 
+console.log("----------------------------------------------------------------------")
+
+function obtermesano(numero) {
+    switch (Number(numero)) {
+        case 1: return "janeiro";
+        case 2: return "fevereiro";
+        case 3: return "março";
+        case 4: return "abril";
+        case 5: return "maio";
+        case 6: return "junho";
+        case 7: return "julho";
+        case 8: return "Agosto";
+        case 9: return "setembro";
+        case 10: return "outubro";
+        case 11: return "novembro";
+        case 12: return "dezembro";
+        default: return "Valor inválido";
+      
+    }
+}
+
+console.log(obtermesano(3)); 
+console.log(obtermesano(6)); 
+console.log(obtermesano(10)); 
+console.log(obtermesano(8)); 
+console.log(obtermesano(14)); 
+console.log("----------------------------------------------------------------------")
+
+var mes = 11;
 
 
 
+
+switch (mes) {
+  case 1:
+  case 2:
+  case 3:
+    console.log("Primeiro trimestre!");
+    break;
+
+
+  case 4:
+  case 5:
+  case 6:
+    console.log("Segundo trimestre!");
+    break;
+    
+  case 7:
+  case 8:
+  case 9:
+    console.log("Terceiro trimestre!");
+    break;
+
+
+  case 10:
+  case 11:
+  case 12:
+    console.log("Quarto trimestre!");
+    break;
+
+
+  default:
+    console.log("Mês incorreto!");
+    break;
+}
+console.log("----------------------------------------------------------------------")
+var operacoes = "somar";
+switch (operacoes) {
+  case "somar":
+    console.log("Você clicou no 'somar'");
+    break;
+  case "subtarir":
+    console.log("Você clicou no 'subtarir'");
+    break;
+  case "multiplicar":
+    console.log("Você clicou no 'multiplicar'");
+    break;
+  case "dividir":
+    console.log("Você clicou no link 'dividir'");
+    break;
+
+}
+console.log("----------------------------------------------------------------------")
 
 
